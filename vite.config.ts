9a347d6daf4c7aa96b1711f298c958dbad56727a
Dennis Rankin <derankinjr@gmail.com>
2024-03-05 14:13:19 -0800

    Update base URL in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/spaceforce-developer-challenge',
+  base: '/space-force-developer-challenge',
   plugins: [
     vue(),
   ],
